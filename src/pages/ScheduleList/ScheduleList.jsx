8478779f68446cb35e25b17fbcff8a40df4cfe36
amirb <amir.bullock9@gmail.com>
2023-08-01 21:45:58 -0400

    add functionality to display the users schedules

--- src/pages/ScheduleList/ScheduleList.jsx
+++ src/pages/ScheduleList/ScheduleList.jsx
@@ -1,12 +1,29 @@
 // css
 import styles from './ScheduleList.module.css'
 
-const ScheduleList = () => {
+function ScheduleList({ schedules }) {
   return (
-    <main className={styles.container}>
-      Schedule List
-    </main>
-  )
+    <div className={styles.container}>
+    <ul>
+      {schedules.map((schedule) => (
+        <li key={schedule._id}>
+          <p>Start Date: {schedule.startDate}</p>
+          <p>End Date: {schedule.endDate}</p>
+          <p>Custom Labels: {schedule.customLabels.join(', ')}</p>
+          <p>Order Receipts:</p>
+          <ul>
+            {schedule.orderReceipts.map((receipt) => (
+              <li key={receipt._id}>
+                <p>Label: {receipt.label}</p>
+                <p>Value: {receipt.value}</p>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+      </div>
+  );
 }
 
 export default ScheduleList
